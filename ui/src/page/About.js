9c5b43b486e2cb4afc4a8e282655cc3246a70002
Luke Emmanuel .C <124929220+Ranking-Topnotch@users.noreply.github.com>
2023-12-30 14:43:53 +0100

    second repo

--- ui/src/page/About.js
+++ ui/src/page/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='flex justify-center items-center flex-col my-5'>
+      <h1 className='text-2xl'>Contact me: </h1>
+      <p><span className='text-red-500 font-bold'>Email:</span> emmaranking07@gmail.com</p>
+    </div>
   )
 }
 
--- ui/src/page/Contact.js
+++ ui/src/page/Contact.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='flex justify-center items-center flex-col my-5'>
+      <h1 className='text-2xl'>About US: </h1>
+      <p className='text-blue-500 font-bold'>We value our customers. Thanks for stoping by</p>
+      <p className='text-yellow-500 font-bold text-4xl my-40'>TR STORE</p>
+    </div>
   )
 }
 
